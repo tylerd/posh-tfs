55de3a4ac402301dd280a62fa171ace1b1faca79
Tyler D <tylergd@outlook.com>
2012-10-25 16:21:51 -0500

    Add Solution

---
+++ TfsPowershell/TfsPowershell/Cmdlets/GetTfsProjectCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace TfsPowershell.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "TfsProjectCollection")]
+    public class GetTfsProjectCollection : PSCmdlet
+    {
+    }
+}
---
+++ TfsPowershell/TfsPowershell/Cmdlets/SetTfsProjectCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+
+namespace TfsPowershell.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Set, "TfsProjectCollection")]
+    public class SetTfsProjectCollection : PSCmdlet
+    {
+    }
+}
